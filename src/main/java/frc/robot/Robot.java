c11f32e0488b3751cccfdff854e9fafccdb0719d
rm0208 <rm0208@gmail.com>
2019-01-13 09:22:03 -0500

    Test

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,43 +1,26 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
-/* import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.PWMVictorSPX;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive; */
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.SerialPort.Port;
 
-/**
- * This is a demo program showing the use of the RobotDrive class, specifically
- * it contains the code necessary to operate a robot with tank drive.
- */
 public class Robot extends TimedRobot {
-  /* private DifferentialDrive m_myRobot;
-  private Joystick m_leftStick;
-  private Joystick m_rightStick; */
-  private SerialPort serialport;
-  
+
+  private SerialPort serialportkUSB1;
+  private String serialString;
 
   @Override
   public void robotInit() {
-/*     m_myRobot = new DifferentialDrive(new PWMVictorSPX(0), new PWMVictorSPX(1));
-    m_leftStick = new Joystick(0);
-    m_rightStick = new Joystick(1); */
-    serialport = new SerialPort(115200, Port.kUSB1);
+
+    serialportkUSB1 = new SerialPort(115200, Port.kUSB1);
+    
   }
 
   @Override
   public void teleopPeriodic() {
-    /* m_myRobot.tankDrive(m_leftStick.getY(), m_rightStick.getY()); */
 
-    System.out.print(serialport.readString());
+    serialString = serialportkUSB1.readString();
+    System.out.println(serialString);
 
   }
 }
